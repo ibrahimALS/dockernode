8f0b1954e9a03e4b0f1da0fe3cb917f068b7e45f
ibrahim alsmery <54236016+ibrahimalsmery@users.noreply.github.com>
2023-08-10 17:44:14 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
 const { signup, login } = require('../controllers/userController');
-
 router.post('/signup', signup)
-
 router.post('/login', login)
-
 module.exports = router;
