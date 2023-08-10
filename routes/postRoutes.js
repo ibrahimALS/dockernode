f2839b6b9312d29c72b3b4205aff63c5aa6b77f4
ibrahim alsmery <54236016+ibrahimalsmery@users.noreply.github.com>
2023-08-10 17:44:01 +0300

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,6 @@
 const { getAllPosts, createPost, updatePost, deletePost, getOnePost } = require('../controllers/postController');
 const { authMiddleware } = require('../middleware/authMiddleare');
-
 const router = require('express').Router();
-
 router.route('/').get(getAllPosts).post(authMiddleware, createPost);
-
 router.route('/:id').get(getOnePost).patch(authMiddleware, updatePost).delete(authMiddleware, deletePost);
-
 module.exports = router;
