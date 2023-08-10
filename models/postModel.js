72e4068815dc38de9f6401ef8f025b9b771977c0
ibrahim alsmery <54236016+ibrahimalsmery@users.noreply.github.com>
2023-08-10 17:45:21 +0300

    Update postModel.js

--- models/postModel.js
+++ models/postModel.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
-
 const postSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, 'Post must have title']
     },
-
     body: {
         type: String,
         required: [true, 'Post must have body']
     }
 })
-
 const postModel = mongoose.model("Post", postSchema);
-
 module.exports = postModel;
